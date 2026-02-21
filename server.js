cb44f5afee492a88b8ef7b5920064a611121eec5
Mrkod-ER <rohitgurjar.khari@gmail.com>
2026-02-21 15:17:18 +0530

    env added

--- server.js
+++ server.js
@@ -25,7 +25,10 @@ io.on('connection', (socket) => {
 });
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    credentials: true
+}));
 app.use(express.json());
 
 // Routes
